d799c3d69aa7b9cefe7a69c8d1e62ab21d34467d
KnightTheLion <joshuaknightmitchell@gmail.com>
2025-08-05 03:20:57 -0400

    updated init.ts skip .git and node_modules when scanning for existing files

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -20,10 +20,18 @@ interface InitOptions {
   packageManager?: PackageManager;
 }
 
-async function findConflicts(src: string, dest: string): Promise<string[]> {
+export const IGNORED_DIRECTORIES = [".git", "node_modules"];
+
+export async function findConflicts(
+  src: string,
+  dest: string
+): Promise<string[]> {
   const entries = await fs.readdir(src, { withFileTypes: true });
   const conflicts: string[] = [];
   for (const entry of entries) {
+    if (entry.isDirectory() && IGNORED_DIRECTORIES.includes(entry.name)) {
+      continue;
+    }
     const srcPath = path.join(src, entry.name);
     const destPath = path.join(dest, entry.name);
     if (await fs.pathExists(destPath)) {
