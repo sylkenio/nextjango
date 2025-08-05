fe9be2c4f7df243c85345fdfc54355bc0fdfde75
KnightTheLion <joshuaknightmitchell@gmail.com>
2025-08-05 04:30:58 -0400

    implemented util function for CORS allowed origins test

--- tests/corsAllowedOrigins.test.ts
+++ tests/corsAllowedOrigins.test.ts
@@ -1,13 +1,10 @@
 import { describe, it, expect, test } from "vitest";
 import { execa } from "execa";
 import path from "path";
+import { isPythonAvailable } from "./utils/python";
 
-let hasPython = true;
-try {
-  await execa("python", ["--version"], { shell: false });
-} catch {
-  hasPython = false;
-}
+const hasPython = await isPythonAvailable();
+
 if (!hasPython) {
   test.skip("Python is required to run CORS origin tests", () => {});
 } else {
